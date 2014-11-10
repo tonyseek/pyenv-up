dc136466299b2a683dced3863645f2790437e5a7
Jiangge Zhang <tonyseek@gmail.com>
2014-11-10 23:16:29 +0800

    set up pyenv in local.

--- run-tests.sh
+++ run-tests.sh
@@ -2,4 +2,7 @@
 
 set -e
 cd "`dirname $0`"
+[ -d "tests/pyenv" ] || git clone https://github.com/yyuu/pyenv.git tests/pyenv
+export PYENV_ROOT="${PWD}/tests/pyenv"
+mkdir -p "${PYENV_ROOT}/virtualenvs"
 bats $@ tests
